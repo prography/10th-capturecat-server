0a0e5d6a170d2b7bb7b4b43676dfbdbc65ec553e
leeedohyun <ldh19738@gmail.com>
2025-06-08 16:12:58 +0900

    feat: core 모듈 생성

--- capturecat-core/src/test/java/com/capturecat/CaptureCatApplicationTest.java
+++ capturecat-core/src/test/java/com/capturecat/CaptureCatApplicationTest.java
@@ -1,13 +1,13 @@
-package com.capturecat.server;
+package com.capturecat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class CaptureCatApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
