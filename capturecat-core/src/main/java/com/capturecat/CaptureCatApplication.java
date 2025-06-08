e4fd78a8b1e2872d9a86079c4983d4382928110c
leeedohyun <ldh19738@gmail.com>
2025-06-08 20:42:33 +0900

    feat: 초기 모듈 셋팅 (#4)
    * feat: core 모듈 생성
    
    * feat: support 모듈 생성
    
    * feat: api-docs 모듈 생성
    
    * feat: clients 모듈 생성
    
    * feat: 헬스 체크 추가

---
+++ capturecat-core/src/main/java/com/capturecat/CaptureCatApplication.java
@@ -0,0 +1,13 @@
+package com.capturecat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptureCatApplication.class, args);
+    }
+
+}
