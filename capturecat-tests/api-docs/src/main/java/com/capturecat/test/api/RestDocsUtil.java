d5ca2d13720a2f4f2e802e47c877e45b0eefdf1d
leeedohyun <ldh19738@gmail.com>
2025-06-10 21:45:45 +0900

    feat: Spring Rest Docs 설정

---
+++ capturecat-tests/api-docs/src/main/java/com/capturecat/test/api/RestDocsUtil.java
@@ -0,0 +1,16 @@
+package com.capturecat.test.api;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+public class RestDocsUtil {
+
+    public static OperationRequestPreprocessor requestPreprocessor() {
+        return Preprocessors.preprocessRequest(Preprocessors.prettyPrint());
+    }
+
+    public static OperationResponsePreprocessor responsePreprocessor() {
+        return Preprocessors.preprocessResponse(Preprocessors.prettyPrint());
+    }
+}
