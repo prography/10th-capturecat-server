40f39b78a37b149a256e5c315a99c75f4202b762
leeedohyun <ldh19738@gmail.com>
2025-06-14 22:44:33 +0900

    feat: 태그 도메인 설계

---
+++ capturecat-core/src/main/java/com/capturecat/core/domain/tag/ImageTag.java
@@ -0,0 +1,28 @@
+package com.capturecat.core.domain.tag;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import com.capturecat.core.domain.image.Image;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ImageTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Image image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tag tag;
+}
---
+++ capturecat-core/src/main/java/com/capturecat/core/domain/tag/Tag.java
@@ -0,0 +1,20 @@
+package com.capturecat.core.domain.tag;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
