c7fa2ad813e68a4e516da996fddefb886f4fd3d9
leeedohyun <ldh19738@gmail.com>
2025-06-15 15:18:40 +0900

    refactor: 태그 개수 검증 컴포넌트 분리

--- capturecat-core/src/main/java/com/capturecat/core/service/image/ImageService.java
+++ capturecat-core/src/main/java/com/capturecat/core/service/image/ImageService.java
@@ -13,6 +13,7 @@ import com.capturecat.core.domain.tag.ImageTag;
 import com.capturecat.core.domain.tag.ImageTagFactory;
 import com.capturecat.core.domain.tag.ImageTagRepository;
 import com.capturecat.core.domain.tag.Tag;
+import com.capturecat.core.domain.tag.TagMaxCountValidator;
 import com.capturecat.core.domain.tag.TagRepository;
 import com.capturecat.core.support.error.CoreException;
 import com.capturecat.core.support.error.ErrorType;
@@ -23,30 +24,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ImageService {
 
-    private static final int TAG_MAX_COUNT = 4;
-
     private final ImageRepository imageRepository;
     private final ImageTagRepository imageTagRepository;
     private final TagRepository tagRepository;
     private final ImageTagFactory imageTagFactory;
+    private final TagMaxCountValidator tagMaxCountValidator;
 
-    // TODO: tagNames 수에 대한 검증
-    // TODO: tagNames 중복 제거
     @Transactional
     public void addTagsToImage(Long imageId, List<String> tagNames) {
         Image image = imageRepository.findById(imageId)
                 .orElseThrow(() -> new CoreException(ErrorType.IMAGE_NOT_FOUND));
 
         Set<String> existingTagNames = new HashSet<>(imageTagRepository.findTagNamesByImage(image));
-        List<String> tagsToAdd = tagNames.stream()
-                .filter(tagName -> !existingTagNames.contains(tagName))
-                .toList();
-        int totalCount = existingTagNames.size() + tagsToAdd.size();
-        if (totalCount > TAG_MAX_COUNT) {
-            throw new CoreException(ErrorType.TOO_MANY_TAGS);
-        }
 
-        List<Tag> newTags = tagsToAdd.stream()
+        tagMaxCountValidator.validate(existingTagNames, tagNames);
+
+        List<Tag> newTags = tagNames.stream()
+                .filter(tagName -> !existingTagNames.contains(tagName))
                 .map(Tag::new)
                 .toList();
 
