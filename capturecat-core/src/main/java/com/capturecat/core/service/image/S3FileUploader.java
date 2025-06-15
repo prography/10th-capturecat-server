e23b94abcb4d1f98b18bc2119925db87c4fcab02
Jealyang <jaelyangchoi@gmail.com>
2025-06-15 16:47:51 +0900

    feat: S3 파일 저장 구현

---
+++ capturecat-core/src/main/java/com/capturecat/core/service/image/S3FileUploader.java
@@ -0,0 +1,43 @@
+package com.capturecat.core.service.image;
+
+import com.capturecat.core.support.S3Properties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+@Profile("!local")
+@RequiredArgsConstructor
+public class S3FileUploader extends AbstractFileUploader {
+
+    private final S3Client s3Client;
+    private final S3Properties s3Properties;
+
+    @Override
+    public String upload(MultipartFile file) throws IOException {
+        String key = buildKey(s3Properties.dirPrefix(), file);
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(s3Properties.bucket())
+                .key(key)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+
+        return String.join("/", s3Properties.urlPrefix(), key);
+    }
+
+
+
+
+}
---
+++ capturecat-core/src/main/java/com/capturecat/core/support/S3Properties.java
@@ -0,0 +1,16 @@
+package com.capturecat.core.support;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
+import org.springframework.validation.annotation.Validated;
+
+
+@Profile("!local")
+@Validated
+@ConfigurationProperties(prefix = "image.s3")
+public record S3Properties(
+        @NotEmpty String bucket,
+        @NotEmpty String dirPrefix,
+        @NotEmpty String urlPrefix
+) { }
