d5de2b2c1977ded28b2029efbe7e75eaa386cf44
Jealyang <jaelyangchoi@gmail.com>
2025-06-27 23:50:31 +0900

    feat: 공통 Bean Validator 적용

---
+++ capturecat-core/src/main/java/com/capturecat/core/support/aop/CommonValidationAdvice.java
@@ -0,0 +1,40 @@
+package com.capturecat.core.support.aop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import com.capturecat.core.support.error.CoreException;
+import com.capturecat.core.support.error.ErrorType;
+
+@Component
+@Aspect
+public class CommonValidationAdvice {
+
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+	public void postMapping() {
+	}
+
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+	public void putMapping() {
+	}
+
+	@Around("(postMapping() || putMapping()) && within(com.capturecat.core.api.user..*)") //우선 user 패키지 하위에만 적용
+	public Object validationAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object[] args = joinPoint.getArgs();
+		for (Object arg : args) {
+			if (arg instanceof BindingResult bindingResult && bindingResult.hasErrors()) {
+				Map<String, String> errorMap = new HashMap<>();
+				bindingResult.getFieldErrors().forEach(err -> errorMap.put(err.getField(), err.getDefaultMessage()));
+				throw new CoreException(ErrorType.VALIDATION_FAIL); //TODO: errorMap 전달
+			}
+		}
+		return joinPoint.proceed();
+	}
+}
--- capturecat-core/src/main/java/com/capturecat/core/support/error/ErrorCode.java
+++ capturecat-core/src/main/java/com/capturecat/core/support/error/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
 	EXAMPLE_ERROR("An example error occurred."), NOT_FOUND_IMAGE("이미지를 찾을 수 없어요."),
 	EXCEED_MAX_TAG_COUNT("태그는 최대 4개까지만 추가할 수 있어요."), S3_FILE_UPLOAD_FAILED("파일 업로드 과정에서 오류가 발생했어요."),
 	INVALID_IMAGE_FORMAT("이미지 파일만 업로드할 수 있어요."), INVALID_REQUEST("잘못된 요청입니다."),
-	ALREADY_EXISTS_USERNAME("이미 존재하는 회원 이름이에요.");
+	ALREADY_EXISTS_USERNAME("이미 존재하는 회원 이름이에요."), BEAN_VALIDATION_FAIL("입력 데이터 오류입니다.");
 	private final String message;
 
 	ErrorCode(String message) {
