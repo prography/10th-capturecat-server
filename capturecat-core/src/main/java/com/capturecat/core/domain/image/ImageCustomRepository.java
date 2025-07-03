7bac0bab7311eb0ffe6729ecf25241d6887c16a6
leeedohyun <ldh19738@gmail.com>
2025-07-03 10:55:25 +0900

    feat: 태그로 이미지 검색 API (#54)
    * refactor: 잘못된 순서 변경
    
    * feat: 태그로 이미지 검색 API
    
    * docs: 태그 검색 문서

--- capturecat-core/src/main/java/com/capturecat/core/domain/image/ImageCustomRepository.java
+++ capturecat-core/src/main/java/com/capturecat/core/domain/image/ImageCustomRepository.java
@@ -1,5 +1,7 @@
 package com.capturecat.core.domain.image;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
@@ -9,4 +11,6 @@ import com.capturecat.core.domain.user.User;
 public interface ImageCustomRepository {
 
 	Slice<ImageInfo> searchByUser(User user, Pageable pageable);
+
+	Slice<ImageInfo> searchImagesByUserAndTagNames(User user, List<String> tagNames, Pageable pageable);
 }
