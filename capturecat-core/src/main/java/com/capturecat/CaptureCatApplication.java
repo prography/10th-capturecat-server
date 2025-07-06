8faf9ba69024b2278d43272e786b389cb97ec26c
Jealyang <jaelyangchoi@gmail.com>
2025-07-07 03:01:09 +0900

    refactor: JPA Auditing이 test code 시 동작하지 않도록 분리

--- capturecat-core/src/main/java/com/capturecat/CaptureCatApplication.java
+++ capturecat-core/src/main/java/com/capturecat/CaptureCatApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan(basePackages = "com.capturecat.core.support")
-@EnableJpaAuditing
 @SpringBootApplication
 public class CaptureCatApplication {
 
---
+++ capturecat-core/src/main/java/com/capturecat/core/config/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package com.capturecat.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@Profile("!test")
+public class JpaAuditingConfig {
+}
