87cf52ff8d7e0b77106d2f7af73a5b105f18b0f0
Jealyang <jaelyangchoi@gmail.com>
2025-07-18 23:58:42 +0900

    fix: 엑세스키 만료시간 복구

--- capturecat-clients/uploader/src/main/java/com/capturecat/client/upload/config/FileUploaderConfig.java
+++ capturecat-clients/uploader/src/main/java/com/capturecat/client/upload/config/FileUploaderConfig.java
@@ -15,7 +15,9 @@ public class FileUploaderConfig {
 	@Bean
 	@Profile({ "dev", "prod" })
 	public S3Client s3Client() {
-		return S3Client.builder().region(Region.AP_NORTHEAST_2).build(); // ~/.aws/credentials의 default 프로파일 사용
+		// AWS SDK는 AWS_ACCESS_KEY를 자동 인식
+		// EC2 환경 변수 또는 ~/.aws/credentials, IAM Role 등 외부에 저장된 키를 사용함
+		return S3Client.builder().region(Region.AP_NORTHEAST_2).build();
 	}
 
 }
