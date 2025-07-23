a9bf8acd30f93a02e7dd8ce5a8507301a5a58671
Jealyang <jaelyangchoi@gmail.com>
2025-07-23 23:53:31 +0900

    feat: 회원 탈퇴(회원만)

--- capturecat-core/src/main/java/com/capturecat/core/service/user/UserService.java
+++ capturecat-core/src/main/java/com/capturecat/core/service/user/UserService.java
@@ -43,11 +43,30 @@ public class UserService {
 	/**
 	 * 소셜 로그인 시 회원가입 처리
 	 * TODO: PROVIDER와 SUBJECT를 기준으로 '소셜서비스'+'소셜ID' 형태로 찾는다. 각기 다른 소셜로그인으로 로그인했을 때 문제가 될 것 같다.
+	 * => 소셜 로그인 매핑 테이블 만들 것. (구글, 애플, 카카오 세개 전부 가능하다) + 회원 탈퇴 시 cascade 삭제
 	 */
 	public LoginUser upsertSocialUser(OidcUserPayload payload) {
 		User user = userRepository.findByProviderAndSocialId(payload.provider(), payload.sub())
 			.orElseGet(() -> userRepository.save(buildUser(payload)));
 		return new LoginUser(user);
+	}
+
+	/**
+	 * 튜토리얼(시작하기) 완료 상태 저장
+	 */
+	public void updateTutorialCompleted(LoginUser loginUser) {
+		User user = userRepository.findByUsername(loginUser.getUsername()) //email
+			.orElseThrow(() -> new CoreException(ErrorType.USER_NOT_FOUND));
+		user.tutorialComplete();
+	}
+
+	/**
+	 * 회원 탈퇴
+	 */
+	public void withdraw(LoginUser loginUser) {
+		User user = userRepository.findByUsername(loginUser.getUsername()) //email
+			.orElseThrow(() -> new CoreException(ErrorType.USER_NOT_FOUND));
+		userRepository.delete(user);
 	}
 
 	private User buildUser(OidcUserPayload payload) {
@@ -59,11 +78,5 @@ public class UserService {
 			.socialId(payload.sub())
 			.role(UserRole.USER)
 			.build();
-	}
-
-	public void updateTutorialCompleted(LoginUser loginUser) {
-		User user = userRepository.findByUsername(loginUser.getUsername())
-			.orElseThrow(() -> new CoreException(ErrorType.USER_NOT_FOUND));
-		user.tutorialComplete();
 	}
 }
--- capturecat-core/src/test/java/com/capturecat/core/service/user/UserServiceTest.java
+++ capturecat-core/src/test/java/com/capturecat/core/service/user/UserServiceTest.java
@@ -2,14 +2,16 @@ package com.capturecat.core.service.user;
 
 import static com.capturecat.core.DummyObject.*;
 import static com.capturecat.core.api.user.dto.UserReqDto.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -19,6 +21,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.capturecat.core.domain.user.User;
 import com.capturecat.core.domain.user.UserRepository;
 import com.capturecat.core.service.auth.LoginUser;
+import com.capturecat.core.support.error.CoreException;
+import com.capturecat.core.support.error.ErrorType;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -66,5 +70,40 @@ class UserServiceTest {
 
 		//then
 		Assertions.assertEquals(true, savedUser.isTutorialCompleted());
+	}
+
+	@Test
+	void 회원탈퇴_성공() {
+		//given
+		//기 회원 만들기
+		User savedUser = newMockUser(1L);
+
+		when(userRepository.findByUsername(savedUser.getUsername())).thenReturn(Optional.of(savedUser));
+
+		//when
+		userService.withdraw(new LoginUser(savedUser));
+
+		//then
+		ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+		verify(userRepository).delete(captor.capture());
+		assertThat(captor.getValue()).isSameAs(savedUser);
+	}
+
+	@Test
+	void 존재하지_않는_회원_탈퇴시_예외() {
+		// given
+		//given
+		//기 회원 만들기
+		User savedUser = newMockUser(1L);
+
+		when(userRepository.findByUsername(savedUser.getUsername())).thenReturn(Optional.empty());
+
+		// when & then
+		assertThatThrownBy(() -> userService.withdraw(new LoginUser(savedUser)))
+			.isInstanceOf(CoreException.class)
+			.satisfies(e -> {
+				CoreException ce = (CoreException) e;
+				assertThat(ce.getErrorType()).isEqualTo(ErrorType.USER_NOT_FOUND);
+			});
 	}
 }
