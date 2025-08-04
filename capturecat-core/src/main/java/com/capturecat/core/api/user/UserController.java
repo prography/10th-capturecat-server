0eaaa4a2e26c3ff42a910adde7ea7c23c7b2fde5
Jealyang <jaelyangchoi@gmail.com>
2025-08-04 23:29:36 +0900

    feat: 탈퇴 시 블랙리스트 토큰 저장 적용

--- capturecat-core/src/main/java/com/capturecat/core/api/user/UserController.java
+++ capturecat-core/src/main/java/com/capturecat/core/api/user/UserController.java
@@ -2,11 +2,13 @@ package com.capturecat.core.api.user;
 
 import jakarta.validation.Valid;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,30 +29,40 @@ public class UserController {
 	private final UserService userService;
 	private final TokenService tokenService;
 
+	/**
+	 * 일반 회원 가입 (소셜 로그인 x)
+	 */
 	@PostMapping("/join")
 	public ApiResponse<JoinRespDto> join(@RequestBody @Valid JoinReqDto joinReqDto, BindingResult bindingResult) {
 		JoinRespDto joinRespDto = userService.join(joinReqDto);
+
 		return ApiResponse.success(joinRespDto);
 	}
 
+	/**
+	 * 튜토리얼(시작하기) 완료 업데이트
+	 */
 	@PostMapping("/tutorialComplete")
 	public ApiResponse<?> tutorialCompleted(@AuthenticationPrincipal LoginUser loginUser) {
 		userService.updateTutorialCompleted(loginUser);
+
 		return ApiResponse.success();
 	}
 
 	/**
 	 * 탈퇴 API
-	 * 1) 소셜 로그인 연결 해제
-	 * 2) 회원 정보 삭제
 	 */
 	@DeleteMapping("/withdraw")
-	public ApiResponse<?> withdraw(@AuthenticationPrincipal LoginUser loginUser) {
-		// 1) 소셜 계정 연동 해제 및 회원 정보 삭제
+	public ApiResponse<?> withdraw(@AuthenticationPrincipal LoginUser loginUser,
+		@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+		//1. 소셜 계정 연동 해제 및 회원 정보 삭제
 		String resultMessage = userService.withdraw(loginUser);
 
-		// 2) Refresh Token 삭제
+		//2. Refresh Token 삭제
 		tokenService.deleteRefreshTokenByUsername(loginUser.getUsername());
+
+		//3. Access Token 블랙리스트 등록
+		tokenService.blacklistAccessToken(authHeader);
 
 		return ApiResponse.success(resultMessage);
 	}
