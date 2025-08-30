25891a858ca811a7560a431bb351a0741d8dcd44
leeedohyun <ldh19738@gmail.com>
2025-08-30 21:06:40 +0900

    feat: 외래키 매핑

--- capturecat-core/src/main/java/com/capturecat/core/domain/user/UserTag.java
+++ capturecat-core/src/main/java/com/capturecat/core/domain/user/UserTag.java
@@ -5,7 +5,10 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,9 @@ import com.capturecat.core.domain.BaseTimeEntity;
 import com.capturecat.core.domain.tag.Tag;
 
 @Entity
+@Table(name = "user_tag",
+	uniqueConstraints = @UniqueConstraint(name = "uk_user_tag_user_tag", columnNames = {"user_id", "tag_id"})
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserTag extends BaseTimeEntity {
 
@@ -22,8 +28,10 @@ public class UserTag extends BaseTimeEntity {
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User user;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tag_id")
 	private Tag tag;
 }
